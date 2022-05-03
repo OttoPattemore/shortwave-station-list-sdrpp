5c130053afc6dc80a98f44e961eb695dd664d71e
Otto Pattemore <otto.pattemore@gmail.com>
2022-05-04 09:18:31 +1000

    Load and display station list

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <gui/style.h>
 #include <module.h>
 #include <gui/gui.h>
-#include <gui/widgets/symbol_diagram.h>
 
 #include <fstream>
 #include <curl/curl.h>
@@ -18,13 +17,6 @@ SDRPP_MOD_INFO{
     /* Version:         */ 0, 1, 0,
     /* Max instances    */ 1};
 
-size_t gotdata(char *buffer, size_t itemSize, size_t nitems, void *f){
-    size_t bytes = itemSize*nitems;
-    for (uint32_t i = 0; i < bytes; i++){
-        std::cout << buffer[i];
-    }
-    return bytes;
-}
 
 class ShortwaveStationList : public ModuleManager::Instance
 {
@@ -36,7 +28,10 @@ public:
         fftRedrawHandler.ctx = this;
         fftRedrawHandler.handler = fftRedraw;
         gui::waterfall.onFFTRedraw.bindHandler(&fftRedrawHandler);
+
+        
         CURL* curl = curl_easy_init();
+
         if(!curl){
             spdlog::error("[ Shortwave Station List ] Failed to init curl!");
             return;
@@ -44,12 +39,18 @@ public:
 
         // Download data
         curl_easy_setopt(curl, CURLOPT_URL, "https://ottopattemore.github.io/shortwave-station-list/sw.json");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, gotdata);
-
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, ShortwaveStationList::handleData);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, this);
         CURLcode result  = curl_easy_perform(curl);
+
+        // Handle errors
         if(result != CURLE_OK){
             spdlog::error("[ Shortwave Station List ] Failed to download list!");
         }
+
+        // Once the list is download decode the json
+        stationList = json::parse(downloadedData);
+        
 
         // Cleanup
         curl_easy_cleanup(curl);
@@ -76,10 +77,19 @@ public:
     }
 
 private:
+    static size_t handleData(char *buffer, size_t itemSize, size_t nitems, void * ctx)
+    {
+        ShortwaveStationList *_this = (ShortwaveStationList *)ctx;
+        size_t bytes = itemSize * nitems;
+        std::string data;
+        data.resize(bytes);
+        memcpy(data.data(), buffer,bytes);
+        _this->downloadedData.append(data);
+        return bytes;
+    }
     static void menuHandler(void *ctx)
     {
         ShortwaveStationList * _this = (ShortwaveStationList *)ctx;
-        ImGui::Text("Hello!");
     }
     static void fftRedraw(ImGui::WaterFall::FFTRedrawArgs args, void *ctx)
     {
@@ -87,15 +97,18 @@ private:
         std::function<void(std::string name, int freq)> draw = [&args](std::string name, int freq)
         {
             double centerXpos = args.min.x + std::round((freq - args.lowFreq) * args.freqToPixelRatio);
+
             if (freq >= args.lowFreq && freq <= args.highFreq)
             {
                 args.window->DrawList->AddLine(ImVec2(centerXpos, args.min.y), ImVec2(centerXpos, args.max.y), IM_COL32(255, 255, 0, 255));
             }
+
             ImVec2 nameSize = ImGui::CalcTextSize(name.c_str());
             ImVec2 rectMin = ImVec2(centerXpos - (nameSize.x / 2) - 5, args.min.y);
             ImVec2 rectMax = ImVec2(centerXpos + (nameSize.x / 2) + 5, args.min.y + nameSize.y);
             ImVec2 clampedRectMin = ImVec2(std::clamp<double>(rectMin.x, args.min.x, args.max.x), rectMin.y);
             ImVec2 clampedRectMax = ImVec2(std::clamp<double>(rectMax.x, args.min.x, args.max.x), rectMax.y);
+
             if (clampedRectMax.x - clampedRectMin.x > 0)
             {
                 args.window->DrawList->AddRectFilled(clampedRectMin, clampedRectMax, IM_COL32(255, 255, 0, 255));
@@ -105,9 +118,11 @@ private:
                 args.window->DrawList->AddText(ImVec2(centerXpos - (nameSize.x / 2), args.min.y), IM_COL32(0, 0, 0, 255), name.c_str());
             }
         };
-        draw("Test1", 100000000);
-        draw("Test2", 101000000);
+        for(const auto& station : _this->stationList["stations"]){
+            draw(station["name"],station["frequency"].get<int>()*1000);
+        }
     }
+    std::string downloadedData;
     json stationList;
     std::string name;
     bool enabled = true;
